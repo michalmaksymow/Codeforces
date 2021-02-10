69c96c221641be7328455247f229f5feb39d62e4
michalmaksymow <borysbrat@gmail.com>
2021-02-10 13:17:02 +0100

    Create 1186A

---
+++ 800/1186A-VusTheCossackAndAContest.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    if (n > m || n > k)
+        cout << "No\n";
+    else
+        cout << "Yes\n";
+    return 0;
+}
