181880277061b612550580f217205cae0ed77eaf
michalmaksymow <borysbrat@gmail.com>
2022-09-10 11:55:08 +0200

    Start implementation of 1722C

---
+++ 800/1722C-WordGame.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+int main() {
+  int cases{0};
+  std::cin >> cases;
+
+  while (--cases) {
+    int words{};
+    std::vector<std::string> guy1, guy2, guy3;
+    std::map<std::string, int> wordCount;
+
+    std::cin >> words;
+  }
+}
