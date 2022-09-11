cc5831897124fa0d92bd2bcdfe9f928a43d6b115
michalmaksymow <borysbrat@gmail.com>
2022-09-11 13:50:38 +0200

    Complete 1722C

--- 800/1722C-WordGame.cpp
+++ 800/1722C-WordGame.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <iostream>
 #include <map>
 #include <string>
@@ -7,11 +8,32 @@ int main() {
   int cases{0};
   std::cin >> cases;
 
-  while (--cases) {
+  while (cases--) {
     int words{};
-    std::vector<std::string> guy1, guy2, guy3;
-    std::map<std::string, int> wordCount;
+    std::string readWord;
+    std::array<std::vector<std::string>, 3> guys;
+    std::map<std::string, int> wordCounter;
 
     std::cin >> words;
+    for (size_t i = 0; i < 3; ++i) {
+      for (size_t j = 0; j < words; ++j) {
+        std::cin >> readWord;
+        wordCounter[readWord]++;
+        guys[i].push_back(readWord);
+      }
+    }
+
+    for (size_t i = 0; i < 3; ++i) {
+      int score{};
+      for (auto &&word : guys[i]) {
+        if (wordCounter[word] == 1) {
+          score += 3;
+        } else if (wordCounter[word] == 2) {
+          score += 1;
+        }
+      }
+      std::cout << score << ' ';
+    }
+    std::cout << std::endl;
   }
 }
